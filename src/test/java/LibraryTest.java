f2f51827ad1de9188fde0f60947349bbc1637ac9
Michał Wypych <mwypych@agh.edu.pl>
2016-11-17 21:02:55 +0100

    tests splitted into several independent test methods - one method is intentionally failing

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -4,16 +4,42 @@ import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
     @Test
-    public void testFizzBuzzForDifferentInputs() {
+    public void fizzBuzzGiven1ExpectedString1() {
       //setup:
         Library lib = new Library();
       //expect:
         assertEquals("1",lib.fizzBuzz(1));
-        assertEquals("2",lib.fizzBuzz(2));
+    }
+
+    @Test
+    public void fizzBuzzGiven3ExpectedStringFizz() {
+      //setup:
+        Library lib = new Library();
+      //expected:
         assertEquals("Fizz",lib.fizzBuzz(3));
-        assertEquals("4",lib.fizzBuzz(4));
-        assertEquals("Buzz",lib.fizzBuzz(5));
-        assertEquals("Fizz",lib.fizzBuzz(6));
-        assertEquals("FizzBuzz",lib.fizzBuzz(15));
+    }     
+      
+    @Test
+    public void fizzBuzzGiven5ExpectedStringBuzz() {
+      //setup:
+        Library lib = new Library();
+      //expected:
+         assertEquals("Buzz",lib.fizzBuzz(5));
+    }
+
+    @Test
+    public void fizzBuzzGiven15ExpectedStringFizzBuzz() {
+      //setup:
+        Library lib = new Library();
+      //expected:
+         assertEquals("FizzBuzz",lib.fizzBuzz(15));
+    }
+
+    @Test
+    public void fizzBuzzGivenMinus1ExpectedStringEmpty() {
+      //setup:
+        Library lib = new Library();
+      //expected:
+         assertEquals("",lib.fizzBuzz(-1));
     }
 }
